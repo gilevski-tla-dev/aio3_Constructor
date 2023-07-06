9ba4d9016404535868f24148121f0512d4b51da1
Nikita Gilevski <gilevskini2004@gmail.com>
2023-07-06 16:42:52 +0200

    .
    е

---
+++ core/utils/commands.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command='start', description='Начало работы'),
+        BotCommand(command='description', description='Описание бота'),
+        BotCommand(command='new_post', description='Создать новый пост')
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from core.handlers.basic import get_start
+from core.handlers.basic import *
 from core.settings import settings
 from aiogram.filters import Text, Command 
+from core.utils.commands import set_commands
 
 import asyncio
 import logging
 
 async def start_bot(bot: Bot):
+    await set_commands(bot)
     await bot.send_message(settings.bots.admin_id, text='Bot is running')
 
 
@@ -15,11 +17,15 @@ async def start():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=settings.bots.bot_token, parse_mode='HTML')
     dp = Dispatcher()
-
+    
+    dp.startup.register(start_bot)
+    
     dp.message.register(get_start, Command(commands=['start']))
+    dp.message.register(get_start, Command(commands=['new_post']))
     dp.message.register(get_start, Text('Создать новый пост'))
 
-    dp.startup.register(start_bot)
+    dp.message.register(descr_command, Command(commands='description'))
+    
 
     try:
         await dp.start_polling(bot)
