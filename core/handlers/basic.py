56ee4d2927ef84d0e5cfe615ad435717bb62496f
Nikita Gilevski <gilevskini2004@gmail.com>
2023-07-06 16:44:58 +0200

    Update basic.py

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -2,9 +2,9 @@ from aiogram import Bot
 from aiogram.types import Message
 
 async def get_start(message: Message, bot: Bot):
-    await message.answer(f'Приветствую, {message.from_user.first_name}. Для того чтобы создать сайт, придумай <b>ЗАГОЛОВОК</b>')
+    await message.answer(f'Приветствую, {message.from_user.first_name}. Для того чтобы создать сайт, придумай <b>ЗАГОЛОВОК.</b>')
 
 async def descr_command(message: Message, bot: Bot):
-    await message.answer('Благодаря этому боту вы можете размещать на сайте посты, фотографии, менять темы и оформление ') 
+    await message.answer('Благодаря этому боту вы можете размещать на сайте посты, фотографии, менять темы и оформление.') 
 
    
